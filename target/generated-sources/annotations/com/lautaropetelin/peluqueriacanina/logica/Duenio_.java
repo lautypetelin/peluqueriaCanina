ef90fb277d4de58b04aab9f16b6bd0c315fb944a
Lautaro Tomás Israel Petelin <lautisrra@gmail.com>
2024-10-30 20:04:55 -0300

    Nueva IGU (Conservando la anterior)

--- target/generated-sources/annotations/com/lautaropetelin/peluqueriacanina/logica/Duenio_.java
+++ target/generated-sources/annotations/com/lautaropetelin/peluqueriacanina/logica/Duenio_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-30T14:25:09", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-30T20:02:38", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Duenio.class)
 public class Duenio_ { 
 
